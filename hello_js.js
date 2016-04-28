e273a06925d0a6ae5db21f8d87eb77dfe1d8fe42
Sasha Egorov <a.a.egoroff@gmail.com>
2016-04-28 18:34:28 +0400

    Node.js version fixed

--- hello_js.js
+++
@@ -1,9 +0,0 @@
-// Hellos from hello.js
-
-// Pattern 2
-var hello_function_anon = require('./lib/js/hello_function_anon.js')
-console.log(hello_function_anon())
-
-// Pattern 3
-var hello_function = require('./lib/js/hello_function_named.js')
-console.log(hello_function.something_named())
