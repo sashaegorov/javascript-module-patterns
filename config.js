a86b7c3ab86793cbba52cbb985e01cb6a37a8422
Sasha Egorov <a.a.egoroff@gmail.com>
2016-04-28 18:14:38 +0400

    jQuery added as example

--- config.js
+++ config.js
@@ -10,47 +10,24 @@ System.config({
     "dest/main.js": [
       "lib/js/main.js",
       "lib/js/hello_js_browser.js",
+      "npm:jquery@2.2.3.js",
+      "npm:jquery@2.2.3/dist/jquery.js",
       "lib/js/hello_function_named.js",
       "lib/js/hello_function_anon.js"
     ]
   },
 
   map: {
+    "jquery": "npm:jquery@2.2.3",
     "typescript": "npm:typescript@1.8.10",
-    "zepto": "npm:zepto@1.0.1",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
-    },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
-    },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
-    },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:assert@1.3.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:inherits@2.0.1": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:os-browserify@0.1.2": {
       "os": "github:jspm/nodelibs-os@0.1.0"
     },
-    "npm:process@0.11.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
-    },
     "npm:typescript@1.8.10": {
       "os": "github:jspm/nodelibs-os@0.1.0"
-    },
-    "npm:util@0.10.3": {
-      "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:zepto@1.0.1": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
