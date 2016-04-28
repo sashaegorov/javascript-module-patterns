d84885839769d9a9e15efeeb3139b86a80a2d7d1
Sasha Egorov <a.a.egoroff@gmail.com>
2016-04-28 14:53:08 +0400

    "Interface" changed a bit

--- hello_js.js
+++ hello_js.js
@@ -2,8 +2,8 @@
 
 // Pattern 2
 var hello_function_anon = require('./lib/js/hello_function_anon.js')
-hello_function_anon()
+console.log();(hello_function_anon())
 
 // Pattern 3
 var hello_function = require('./lib/js/hello_function_named.js')
-hello_function.something_named()
+console.log(hello_function.something_named())
